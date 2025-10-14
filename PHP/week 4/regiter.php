2c27249abe19c6e9760403cd917cdaf6ecd91b8f
ngocuyen-web <nguyenuyen220904@gmail.com>
2025-10-14 15:55:32 +0700

    week 4

--- PHP/week 4/regiter.php
+++ PHP/week 4/regiter.php
@@ -9,7 +9,6 @@ if (isset($_POST['register'])) {
     if (mysqli_num_rows($check) > 0) {
         echo "Existed";
     } else {
-        
         $query = "INSERT INTO users (username, password) VALUES ('$username', '$password')";
         if (mysqli_query($link, $query)) {
             echo "Successful";
@@ -19,21 +18,26 @@ if (isset($_POST['register'])) {
     }
 }
 ?>
+ 
 <!DOCTYPE html>
 <html lang="en">
 <head>
     <meta charset="UTF-8">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
-    <title>Login</title>
+    <title>Register</title>
 </head>
 <body>
-<h2>Login</h2>
-<form action="" method="post">
-    <label for="username">User name</label>
-    <input type="text" name="username" required><br>
-    <label for="">Password</label>
-    <input type="password" id= ""name="password" required><br>
-    <input type="submit" name="register" value="Register">
-</form>
+    <h2>Register</h2>
+    <form action="" method="post">
+        <label for="username">Username:</label><br>
+        <input type="text" name="username" id="username" required><br><br>
+
+        <label for="password">Password:</label><br>
+        <input type="password" name="password" id="password" required><br><br>
+
+        <input type="submit" name="register" value="Register">
+    </form>
+
+    <p>Already have an account? <a href="login.php">Login here</a></p>
 </body>
 </html>
